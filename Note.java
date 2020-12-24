7eeccae9958da616b659e0dec79d46579c9e1b76
Rajarsi Saha <rajarsi3997@gmail.com>
2020-12-24 21:42:39 +0530

    updates

--- Note.java
+++ Note.java
@@ -1,45 +1,91 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
-public class Note {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Note");
+import java.awt.event.*;
+import java.io.*;
+
+public class Note implements ActionListener {
+    JFrame frame;
+    JScrollPane scrollPane;
+    JTextArea area;
+    JTextField file_name;
+    JButton save;
+    JMenuItem close_item;
+
+    public Note() {
+        frame = new JFrame("Note");
 
         JMenuBar mb = new JMenuBar();
         JMenu file_menu = new JMenu("File");
         JMenu help_menu = new JMenu("Help");
-        JMenuItem close_item = new JMenuItem("Close");
+        close_item = new JMenuItem("Close");
+        close_item.addActionListener(this);
 
         file_menu.add(close_item);
         mb.add(file_menu);
         mb.add(help_menu);
         frame.setJMenuBar(mb);
 
-        JTextArea area = new JTextArea("Please type..");
+        area = new JTextArea("Delete and keep typing...");
         area.setBounds(0, 0, 640, 400);
+        area.setCaretPosition(area.getDocument().getLength());
 
-        JScrollPane scrollPane = new JScrollPane();
         scrollPane = new JScrollPane(area);
         scrollPane.setViewportView(area);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(new EmptyBorder(5, 5, 5, 5));
         panel.setBounds(0, 310, 640, 100);
 
-        JTextField file_name = new JTextField("file0.txt");
-        file_name.setColumns(50);
-        panel.add(file_name);
+        JLabel fileLabel = new JLabel("File Name: ");
+        fileLabel.setBorder(new EmptyBorder(5, 0, 5, 5));
+        panel.add(fileLabel, BorderLayout.WEST);
 
-        JButton b2 = new JButton(" SAVE ");
-        panel.add(b2);
+        JPanel panel1 = new JPanel(new BorderLayout());
+        panel1.setBorder(new EmptyBorder(0, 0, 0, 5));
+        file_name = new JTextField("");
+        panel1.add(file_name, BorderLayout.CENTER);
+        panel.add(panel1, BorderLayout.CENTER);
+
+        save = new JButton(" SAVE ");
+        save.addActionListener(this);
+        panel.add(save, BorderLayout.EAST);
 
         frame.add(panel, BorderLayout.SOUTH);
         
         frame.setSize(640, 480);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == close_item) {
+            frame.dispose();
+            System.exit(1);
+        }
+        else if(e.getSource() == save) {
+            String text = area.getText();
+            String file = file_name.getText();
+            FileWriter fw;
+
+            try {
+                fw = new FileWriter(file + ".txt");
+                fw.write(text);
+                fw.close();
+            }
+            catch(Exception error) {
+                System.err.println(error.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Note();
     }
 }
